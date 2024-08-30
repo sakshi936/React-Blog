c517d171eed3696ee3ba6e28e82a3bbd35311d26
sakshipandey2601 <pandeysakshi2601@gmail.com>
2024-08-30 23:10:15 +0530

    add: container

---
+++ src/components/container/Container.tsx
@@ -0,0 +1,10 @@
+import React, { Children, ReactNode } from "react";
+
+interface childComponent {
+	children: React.ReactNode;
+}
+function Container({ children }: childComponent) {
+	return <div className=" w-full max-w-7xl mx-auto px-4">{children}</div>;
+}
+
+export default Container;
