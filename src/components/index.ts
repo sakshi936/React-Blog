a5853f858faa79c892a6b41a32aee3f2121518b6
sakshipandey2601 <pandeysakshi2601@gmail.com>
2024-10-17 22:36:40 +0530

    add: export container, logo and logoutbtn

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,7 @@
-import Header from "./Header/header";
-import Footer from "./Footer/footer";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./atoms/LogoutBtn";
 
-export { Header, Footer };
+export { Header, Footer, Container, Logo, LogoutBtn };
