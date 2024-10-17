d0a0ab8f8ceae4179ed02311d0cc2e98ce5f1948
sakshipandey2601 <pandeysakshi2601@gmail.com>
2024-10-17 22:37:54 +0530

    add: props types

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+import { ReactNode, ButtonHTMLAttributes, InputHTMLAttributes } from "react";
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+	children: ReactNode; // Children can be any valid React element(ReactNode is a type that represents anything that can be rendered by React: strings, numbers, JSX elements, fragments, or even null.)
+	type?: "button" | "submit" | "reset"; // Restricting type to valid button types
+	bgColor?: string; // Class name or custom styles for background color
+	textColor?: string; // Class name or custom styles for text color
+	className?: string; // Additional class names
+}
+
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+	label?: string;
+	type?: "checkbox" | "email" | "text" | "password" | "submit" | "url" | "image";
+	className?: string;
+}
