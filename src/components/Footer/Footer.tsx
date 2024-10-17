d1d10cd7ea20fc2f311df21296327fcfc2e8c9bc
sakshipandey2601 <pandeysakshi2601@gmail.com>
2024-10-17 22:34:57 +0530

    add: logo

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,5 @@
-import React from "react";
 import { Link } from "react-router-dom";
-// import Logo from "../Logo";
+import Logo from "../Logo";
 
 function Footer() {
 	return (
@@ -9,7 +8,9 @@ function Footer() {
 				<div className="-m-6 flex flex-wrap">
 					<div className="w-full p-6 md:w-1/2 lg:w-5/12">
 						<div className="flex h-full flex-col justify-between">
-							<div className="mb-4 inline-flex items-center">{/* <Logo width="100px" /> */}</div>
+							<div className="mb-4 inline-flex items-center">
+								<Logo width="100px" />
+							</div>
 							<div>
 								<p className="text-sm text-gray-600">&copy; Copyright 2023. All Rights Reserved by DevUI.</p>
 							</div>
