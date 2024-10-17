1e82b8845c596d45adbe0b3d391fbb45e230b86f
sakshipandey2601 <pandeysakshi2601@gmail.com>
2024-10-17 22:32:53 +0530

    add: reusable button component

---
+++ src/components/atoms/Button.tsx
@@ -0,0 +1,12 @@
+import React, { ReactNode } from "react";
+import { ButtonProps } from "../../types";
+
+function Button({ children, type = "button", bgColor = "bg-blue-600", textColor = "text-white", className = "", ...props }: ButtonProps) {
+	return (
+		<button className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`} {...props}>
+			{children}
+		</button>
+	);
+}
+
+export default Button;
