8bc9f45d91a9e4ca3f5e6b7574ab286c3a8a0eb0
sakshipandey2601 <pandeysakshi2601@gmail.com>
2024-10-18 13:37:15 +0530

    faet: select options type

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-import { ReactNode, ButtonHTMLAttributes, InputHTMLAttributes } from "react";
+import { ReactNode, ButtonHTMLAttributes, InputHTMLAttributes, SelectHTMLAttributes } from "react";
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 	children: ReactNode; // Children can be any valid React element(ReactNode is a type that represents anything that can be rendered by React: strings, numbers, JSX elements, fragments, or even null.)
@@ -11,5 +11,11 @@ export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 	label?: string;
 	type?: "checkbox" | "email" | "text" | "password" | "submit" | "url" | "image";
+	className?: string;
+}
+
+export interface SelectBtnProps extends SelectHTMLAttributes<HTMLSelectElement> {
+	options?: [option: string];
+	label?: string;
 	className?: string;
 }
