15a83210377638140112579c3862e18a466469eb
sakshipandey2601 <pandeysakshi2601@gmail.com>
2024-10-18 13:02:43 +0530

    add: select options button

---
+++ src/components/atoms/SelectBtn.tsx
@@ -0,0 +1,21 @@
+import React, { Ref, useId } from "react";
+import { SelectBtnProps } from "../../types";
+
+function SelectBtn({ options, label, className = "", ...props }: SelectBtnProps, ref: Ref<HTMLSelectElement>) {
+	const id = useId();
+
+	return (
+		<div className="w-full">
+			{label && <label htmlFor={id} className=""></label>}
+			<select {...props} id={id} ref={ref} className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}>
+				{options?.map((option) => (
+					<option key={option} value={option}>
+						{option}
+					</option>
+				))}
+			</select>
+		</div>
+	);
+}
+
+export default React.forwardRef(SelectBtn);
